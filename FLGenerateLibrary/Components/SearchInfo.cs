79efcef51575ac6bbf1d2995edc7b13f4626b444
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-02 06:52:21 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-02

--- FLGenerateLibrary/Components/SearchInfo.cs
+++ FLGenerateLibrary/Components/SearchInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 сентября 2025 11:06:37
- * Version: 1.0.7
+ * Last Updated: 02 сентября 2025 06:52:12
+ * Version: 1.0.8
  */
 
 namespace FLGenerateLibrary
