6f03d95094c13d181810a7b7d9ac3ab766d2cef8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-13 10:02:38 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-13

--- FLResources/Properties/OfferBribeResources_name.Designer.cs
+++ FLResources/Properties/OfferBribeResources_name.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 октября 2025 10:59:11
- * Version: 1.0.48
+ * Last Updated: 13 октября 2025 10:02:30
+ * Version: 1.0.49
  */
 
 //------------------------------------------------------------------------------
