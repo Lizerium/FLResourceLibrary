458f134caeac818cf672358eec0ff5bee8553208
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-17 13:18:40 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-17

--- FLCore/Extensions.cs
+++ FLCore/Extensions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 16 октября 2025 10:58:23
- * Version: 1.0.52
+ * Last Updated: 17 октября 2025 13:18:31
+ * Version: 1.0.53
  */
 
 namespace FLCore
