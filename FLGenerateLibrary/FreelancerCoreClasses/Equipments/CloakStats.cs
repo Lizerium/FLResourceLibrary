d51f8d2246152102086356ec662a4db567ba7e7c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-20 12:22:27 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-20

--- FLGenerateLibrary/FreelancerCoreClasses/Equipments/CloakStats.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Equipments/CloakStats.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 ноября 2025 08:16:08
- * Version: 1.0.86
+ * Last Updated: 20 ноября 2025 12:22:18
+ * Version: 1.0.87
  */
 
 using System;
