396ead592d93691d0deb868db022579ee2515a52
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-08 13:07:27 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-08

--- FLResources/IResources.cs
+++ FLResources/IResources.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 декабря 2025 12:57:04
- * Version: 1.0.104
+ * Last Updated: 08 декабря 2025 13:07:18
+ * Version: 1.0.105
  */
 
 
