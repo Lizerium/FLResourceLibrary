f2f506e623e930afc86c81754defbfcf458036c2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-11 14:04:25 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-11

--- FLGenerateLibrary/FreelancerCoreClasses/Equipments/MineDropperStats.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Equipments/MineDropperStats.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 декабря 2025 06:52:09
- * Version: 1.0.107
+ * Last Updated: 11 декабря 2025 14:04:16
+ * Version: 1.0.108
  */
 
 using System;
