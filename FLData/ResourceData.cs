585fef8facec33972ce474564b669654bbf26424
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-25 09:31:04 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-25

--- FLData/ResourceData.cs
+++ FLData/ResourceData.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 декабря 2025 09:29:33
- * Version: 1.0.121
+ * Last Updated: 25 декабря 2025 09:30:55
+ * Version: 1.0.122
  */
 
 using System.Collections.Generic;
