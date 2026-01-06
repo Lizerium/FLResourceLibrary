4efbad193c7310d5f4a48a19ebde355acc63a07b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-06 06:52:26 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-06

--- FLGenerateLibrary/FreelancerCoreClasses/Equipments/MotorStats.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Equipments/MotorStats.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 января 2026 15:47:04
- * Version: 1.0.133
+ * Last Updated: 06 января 2026 06:52:17
+ * Version: 1.0.134
  */
 
 using System;
