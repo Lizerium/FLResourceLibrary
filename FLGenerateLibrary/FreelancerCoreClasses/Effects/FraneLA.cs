82a5018fb47cebcf8906ff63b66cd1ef9d6b3aba
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-07 10:24:16 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-07

--- FLGenerateLibrary/FreelancerCoreClasses/Effects/FraneLA.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Effects/FraneLA.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 января 2026 06:52:17
- * Version: 1.0.134
+ * Last Updated: 07 января 2026 10:24:06
+ * Version: 1.0.135
  */
 
 using System;
