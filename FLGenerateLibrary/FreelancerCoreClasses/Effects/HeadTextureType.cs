775b6179c2c9e62557bca3d1659b6958fc00da08
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-08 11:24:53 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-08

--- FLGenerateLibrary/FreelancerCoreClasses/Effects/HeadTextureType.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Effects/HeadTextureType.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 января 2026 10:24:06
- * Version: 1.0.135
+ * Last Updated: 08 января 2026 11:24:46
+ * Version: 1.0.136
  */
 
 using System;
