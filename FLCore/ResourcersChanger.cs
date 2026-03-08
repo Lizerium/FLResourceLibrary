dc43475638a78a3e3a02b0807b2520c35332ea38
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-08 09:56:33 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-08

--- FLCore/ResourcersChanger.cs
+++ FLCore/ResourcersChanger.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 марта 2026 06:52:22
- * Version: 1.0.194
+ * Last Updated: 08 марта 2026 09:56:24
+ * Version: 1.0.195
  */
 
 using System.Collections;
