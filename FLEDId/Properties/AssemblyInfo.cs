6bb478dfc77be5668da42e47aa1c6d32612f661e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-23 13:11:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-23

--- FLEDId/Properties/AssemblyInfo.cs
+++ FLEDId/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿/*
  * Author: Nikolay Dvurechensky
- * Site: https://sites.google.com/view/dvurechensky
+ * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 марта 2026 07:55:58
- * Version: 1.0.209
+ * Last Updated: 23 марта 2026 13:11:08
+ * Version: 1.0.210
  */
 
 using System.Reflection;
