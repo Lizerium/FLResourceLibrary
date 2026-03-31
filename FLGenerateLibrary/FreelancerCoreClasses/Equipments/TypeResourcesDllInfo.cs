9fbfe94827894837349c75b0cc40e5ae33bcbb71
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-31 10:53:30 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-31

--- FLGenerateLibrary/FreelancerCoreClasses/Equipments/TypeResourcesDllInfo.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Equipments/TypeResourcesDllInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 марта 2026 12:20:52
- * Version: 1.0.217
+ * Last Updated: 31 марта 2026 10:53:21
+ * Version: 1.0.218
  */
 
 using System;
