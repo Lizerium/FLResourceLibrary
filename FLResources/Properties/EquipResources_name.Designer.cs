d3941a7bf82662c9af3b69fc428aff5640a14d95
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-05 15:04:16 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-05

--- FLResources/Properties/EquipResources_name.Designer.cs
+++ FLResources/Properties/EquipResources_name.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 апреля 2026 12:59:11
- * Version: 1.0.223
+ * Last Updated: 05 апреля 2026 15:04:08
+ * Version: 1.0.224
  */
 
 //------------------------------------------------------------------------------
