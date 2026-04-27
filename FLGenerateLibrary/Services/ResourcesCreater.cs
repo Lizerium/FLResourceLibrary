2d8f8d39abe568eb9d5ecfc5e375e47c53b42e56
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-27 09:41:08 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-27

--- FLGenerateLibrary/Services/ResourcesCreater.cs
+++ FLGenerateLibrary/Services/ResourcesCreater.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 апреля 2026 09:56:35
- * Version: 1.0.247
+ * Last Updated: 27 апреля 2026 09:40:59
+ * Version: 1.0.248
  */
 
 using Core;
