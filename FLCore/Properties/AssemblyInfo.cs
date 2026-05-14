a712d440dfaf3157a8137268857638b3be2bbf38
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-14 10:37:00 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-14

--- FLCore/Properties/AssemblyInfo.cs
+++ FLCore/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 мая 2026 06:51:58
- * Version: 1.0.264
+ * Last Updated: 14 мая 2026 10:36:51
+ * Version: 1.0.265
  */
 
 using System.Reflection;
