24bef569c13dc75a56b1dae8750c1658f26e1b39
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-27 13:25:47 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-27

--- FLGenerateLibrary/FreelancerCoreClasses/Effects/EffectMain.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Effects/EffectMain.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 мая 2026 11:42:44
- * Version: 1.0.277
+ * Last Updated: 27 мая 2026 13:25:38
+ * Version: 1.0.278
  */
 
 using UnityEngine.VFX;
