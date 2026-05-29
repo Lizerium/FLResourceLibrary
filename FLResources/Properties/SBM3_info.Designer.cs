78ac4988b4f0635cb0143a9a74f71dcef55feac9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-29 13:54:37 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-29

--- FLResources/Properties/SBM3_info.Designer.cs
+++ FLResources/Properties/SBM3_info.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 мая 2026 17:30:11
- * Version: 1.0.279
+ * Last Updated: 29 мая 2026 13:54:28
+ * Version: 1.0.280
  */
 
 //------------------------------------------------------------------------------
