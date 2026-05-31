74c3177759f4dbeaef11c4e50da263cda6413cbf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-31 14:55:01 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-31

--- FLResources/Properties/MiscText_info.Designer.cs
+++ FLResources/Properties/MiscText_info.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 мая 2026 15:42:46
- * Version: 1.0.281
+ * Last Updated: 31 мая 2026 14:54:54
+ * Version: 1.0.282
  */
 
 //------------------------------------------------------------------------------
