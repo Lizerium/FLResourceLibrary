d5016548e0caf96ddd03cc31d7f100ad3ea66a35
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-21 06:52:07 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-21

--- FLResources/Properties/InfoCards_info.Designer.cs
+++ FLResources/Properties/InfoCards_info.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 июня 2026 06:51:54
- * Version: 1.0.302
+ * Last Updated: 21 июня 2026 06:51:58
+ * Version: 1.0.303
  */
 
 //------------------------------------------------------------------------------
