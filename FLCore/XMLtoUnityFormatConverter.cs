342b1c2129bd2048e29fedbdfa88474123e08ab7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-29 06:52:35 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-29

--- FLCore/XMLtoUnityFormatConverter.cs
+++ FLCore/XMLtoUnityFormatConverter.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 июня 2026 11:42:27
- * Version: 1.0.310
+ * Last Updated: 29 июня 2026 06:52:25
+ * Version: 1.0.311
  */
 
 using System.Xml;
