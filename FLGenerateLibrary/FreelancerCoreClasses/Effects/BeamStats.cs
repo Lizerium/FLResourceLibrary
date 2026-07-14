f2dd759f1aa354ef033e27eaf182bcbb7afef9a3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-14 06:52:37 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-14

--- FLGenerateLibrary/FreelancerCoreClasses/Effects/BeamStats.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Effects/BeamStats.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 июля 2026 06:52:06
- * Version: 1.0.326
+ * Last Updated: 14 июля 2026 06:52:26
+ * Version: 1.0.327
  */
 
 using System;
