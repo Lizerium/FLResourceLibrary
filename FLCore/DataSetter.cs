f5a77482b0d179beff0fbfa8c7899d5c85743431
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-26 15:11:08 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-26

--- FLCore/DataSetter.cs
+++ FLCore/DataSetter.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 июля 2026 14:29:16
- * Version: 1.0.338
+ * Last Updated: 26 июля 2026 15:10:58
+ * Version: 1.0.339
  */
 
 using System.Collections.Generic;
