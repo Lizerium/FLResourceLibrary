7d78303da092192ec1c630c3ce737d51965eb0b8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-28 06:52:13 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-28

--- FLGenerateLibrary/FreelancerCoreClasses/Effects/ExplosionEffectType.cs
+++ FLGenerateLibrary/FreelancerCoreClasses/Effects/ExplosionEffectType.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 августа 2026 08:32:33
- * Version: 1.0.371
+ * Last Updated: 28 августа 2026 06:52:04
+ * Version: 1.0.372
  */
 
 namespace FLGenerateLibrary
