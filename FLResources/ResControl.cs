dd54ae6b652a4da7cec1090b79328fa437dc1047
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-30 06:52:21 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-30

--- FLResources/ResControl.cs
+++ FLResources/ResControl.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 29 августа 2026 06:52:04
- * Version: 1.0.373
+ * Last Updated: 30 августа 2026 06:52:11
+ * Version: 1.0.374
  */
 
 using FLCore;
